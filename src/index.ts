5a55b5cd595b4c6209296bbcbedfbd6785b5521f
Dmitry <Savelyev.ds@yandex.ru>
2025-06-11 00:17:09 +0300

    Change port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 //req.query. обращение к cgi параметрам в запросе
 //req.body. полезная нагрузка POST
